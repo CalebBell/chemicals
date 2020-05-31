57160af164dbad0f73ab1aabe7b9d269623e38e0
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2020-05-31 09:32:26 -0600

    Another documentation change to heat_capacity.py

--- chemicals/heat_capacity.py
+++ chemicals/heat_capacity.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 '''Chemical Engineering Design Library (ChEDL). Utilities for process modeling.
-Copyright (C) 2016, Caleb Bell <Caleb.Andrew.Bell@gmail.com>
+Copyright (C) 2016, 2017, 2018, 2019, 2020 Caleb Bell <Caleb.Andrew.Bell@gmail.com>
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -77,6 +80,7 @@ folder = os.path.join(os.path.dirname(__file__), 'Heat Capacity')
 register_df_source(folder, 'PolingDatabank.tsv')
 register_df_source(folder, 'TRC Thermodynamics of Organic Compounds in the Gas State.tsv')
 register_df_source(folder, 'CRC Standard Thermodynamic Properties of Chemical Substances.tsv')
+
 _Cp_data_loaded = False
 def _load_Cp_data():
     global Cp_data_Poling, TRC_gas_data, CRC_standard_data, Cp_data_PerryI
