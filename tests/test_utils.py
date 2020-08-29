4e51fc2b724de05792bfa0de328da8c6510ea031
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2020-08-29 09:18:10 -0600

    Remove two functions from chemicals whose only use is in thermo

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -258,28 +258,6 @@ def test_SG():
 def test_Watson_K():
     assert_close(Watson_K(400, .8), 11.20351186639291, rtol=1e-13)
 
-def test_allclose_variable():
-    x = [2.7244322249597719e-08, 3.0105683900110473e-10, 2.7244124924802327e-08, 3.0105259397637556e-10, 2.7243929226310193e-08, 3.0104990272770901e-10, 2.7243666849384451e-08, 3.0104101821236015e-10, 2.7243433745917367e-08, 3.0103707421519949e-10]
-    y = [2.7244328304561904e-08, 3.0105753470546008e-10, 2.724412872417824e-08,  3.0105303055834564e-10, 2.7243914341030203e-08, 3.0104819238021998e-10, 2.7243684057561379e-08, 3.0104299541023674e-10, 2.7243436694839306e-08, 3.010374130526363e-10]
-
-    assert allclose_variable(x, y, limits=[.0, .5], rtols=[1E-5, 1E-6])
-        
-    with pytest.raises(Exception):
-        assert allclose_variable(x, y, limits=[.0, .1], rtols=[1E-5, 1E-6])
-        
-    with pytest.raises(Exception):
-        allclose_variable(x, y[1:], limits=[.0, .5], rtols=[1E-5, 1E-6])
-
-    with pytest.raises(Exception):
-        ans = allclose_variable(x, y, limits=[.0, .1])
-
-
-    x = [1,1,1,1,1,1,1,1,1]
-    y = [.9,.9,.9,.9,.9,.9,.9,.9, .9]
-    assert allclose_variable(x, y, limits=[.0], atols=[.1])
-
-
-    
     
 def test_mix_component_flows():
     names, flows = mix_component_flows(['7732-18-5', '64-17-5'], ['7732-18-5', '67-56-1'], 1, 1, [0.5, 0.5], [0.5, 0.5])
