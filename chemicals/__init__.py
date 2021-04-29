5acdc906e615f51ad83ed709b3083fb2a5d91e39
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2021-04-28 21:28:45 -0600

    1.0.7 release

--- chemicals/__init__.py
+++ chemicals/__init__.py
@@ -138,7 +138,7 @@ submodules = [critical, utils, elements, dipole, dippr, temperature, miscdata,
 
 chemicals_dir = utils.source_path
 
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 
 def complete_lazy_loading():
     critical._load_critical_data()
