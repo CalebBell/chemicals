f24eaef15d1c4751f2d7006c664889e4d6e04c5f
Caleb Bell <Caleb.Andrew.Bell@gmail.com>
2023-04-23 15:39:31 -0600

    1.1.3 release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from distutils.core import setup
+from setuptools import setup
 
 classifiers=[
     'Development Status :: 5 - Production/Stable',
